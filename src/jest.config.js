a1669a4ccf41c8bc55741e12b9c7b3ec0adf2906
Alexander Gerbig <agerbig@systhemis.de>
2019-12-30 17:46:47 +0100

    Update of Dependencies

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,15 +1,24 @@
-// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
 module.exports = {
-  preset: 'jest-preset-angular',
-  roots: ['src'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupJest.ts'],
-  moduleNameMapper: {
-    '@app/(.*)': '<rootDir>/src/app/$1',
-    '@assets/(.*)': '<rootDir>/src/assets/$1',
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-    '@env': '<rootDir>/src/environments/environment',
-    '@src/(.*)': '<rootDir>/src/src/$1',
-    '@state/(.*)': '<rootDir>/src/app/state/$1'
+  globals: {
+    "ts-jest": {
+      tsConfig: "<rootDir>/src/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.html$"
+    }
   },
-  transformIgnorePatterns: ['node_modules/(?!(jest-test))']
+  transform: {
+    "^.+\\.(ts|js|html)$": "ts-jest"
+  },
+  testEnvironment: "jest-environment-jsdom-thirteen",
+  moduleFileExtensions: ["ts", "html", "js", "json"],
+  moduleNameMapper: {
+    "^src/(.*)$": "<rootDir>/src/$1",
+    "^app/(.*)$": "<rootDir>/src/app/$1",
+    "^assets/(.*)$": "<rootDir>/src/assets/$1",
+    "^environments/(.*)$": "<rootDir>/src/environments/$1"
+  },
+  transformIgnorePatterns: ["node_modules/(?!@ngrx)"],
+  snapshotSerializers: [
+    "jest-preset-angular/AngularSnapshotSerializer.js",
+    "jest-preset-angular/HTMLCommentSerializer.js"
+  ]
 };
